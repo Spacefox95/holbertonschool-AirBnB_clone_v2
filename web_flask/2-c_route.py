cb34ea5bdd83b1d233abc19c8b1469ac1bd74edf
Spacefox95 <nathan.raynal@gmail.com>
2024-04-08 17:02:59 +0200

    C_is_fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+"""
+Starts Falsk web app and display message
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def hello(text: str):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
