5ad72e2f42d7a1bf7db02de919f202aa1d5cae7d
Spacefox95 <nathan.raynal@gmail.com>
2024-04-09 10:04:51 +0200

    Odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -28,18 +28,39 @@ def c_is_fun(text: str):
 @app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<string:text>", strict_slashes=False)
 def python_text(text: str):
+    """
+    Display “Python ”, followed by the value of the text variable
+     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_display(n: int):
+    """
+    Display “n is a number” only if n is an integer
+    """
     return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def html_display(n: int):
+    """
+    Display a HTML page only if n is an integer
+    """
     return render_template('5-number.html', n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def html_display_odd_even(n: int):
+    """
+    Display a HTML page only if n is an integer
+    """
+    if (n % 2) == 0:
+        parity = "even"
+    else:
+        parity = "odd"
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
 
 
 if __name__ == '__main__':
