d035ecc1af79546c130bf673256e7afa1a9dfe68
Spacefox95 <nathan.raynal@gmail.com>
2024-04-09 09:24:29 +0200

    number_route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+"""
+Starts Falsk web app and display message
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def c_is_fun(text: str):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<string:text>", strict_slashes=False)
+def python_text(text: str):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_display(n: int):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
