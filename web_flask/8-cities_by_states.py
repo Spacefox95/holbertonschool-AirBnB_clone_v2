3b00a174dcbb02173fe243dce9b5ddfc9acdc193
Spacefox95 <nathan.raynal@gmail.com>
2024-04-10 11:44:55 +0200

    pycode

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,15 +12,16 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states/", defaults={'n': 'States'}, strict_slashes=False)
+@app.route("/cities_by_states", defaults={'n': 'States'}, strict_slashes=False)
 @app.route("/cities_by_states/<string:n>", strict_slashes=False)
 def html_page_display(n):
     """
     Display a HTML page only if n is a string
     """
     states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('8-cities_by_states.html', n=n, states=sorted_states)
+    sortd_states = sorted(states, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', n=n, states=sortd_states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
