cf0c96ec1f4ce28130883037f413a97aa040d9f8
Spacefox95 <nathan.raynal@gmail.com>
2024-04-11 13:54:26 +0200

    ok

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,35 +3,29 @@
 Starts Falsk web app and display message
 """
 
-from flask import Flask, render_template, appcontext_tearing_down
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", defaults={'n': 'States'}, strict_slashes=False)
-@app.route("/cities_by_states/<string:n>", strict_slashes=False)
-def html_page_display(n):
+@app.route("/states/", strict_slashes=False)
+@app.route("/states/<id>")
+def html_cities_by_state(id=None):
     """
-    Display a HTML page only if n is a string
+    Display an HTML page listing all cities by state
     """
     states = storage.all(State).values()
-    sortd_states = sorted(states, key=lambda x: x.name)
-    return render_template('8-cities_by_states.html', n=n, states=sortd_states)
-
-
-@app.route('/states/<id>')
-def html_display_titles(id):
-    """ Display HTML page customized id
-    """
-    state_obj = None
-    for state_name in storage.all(State).values():
-        if state_name.id == id:
-            state_obj = state_name
-    return render_template('9-states.html', state_obj=state_obj)
+    sorted_states = sorted(states, key=lambda x: x.name)
+    if id:
+        for state_name in sorted_states:
+            if state_name.id == id:
+                return render_template('9-states.html', item=state_name)
+        return render_template('9-states.html', error="Not Found!")
+    else:
+        return render_template('9-states.html', new_state=sorted_states)
 
 
 @app.teardown_appcontext
