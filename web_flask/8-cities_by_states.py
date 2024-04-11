84ac145f62a7a7bc1af1c3d83d3b74ad0d14dc5d
Spacefox95 <nathan.raynal@gmail.com>
2024-04-11 10:49:40 +0200

    ok

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,24 +3,22 @@
 Starts Falsk web app and display message
 """
 
-from flask import Flask, render_template, appcontext_tearing_down
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", defaults={'n': 'States'}, strict_slashes=False)
-@app.route("/cities_by_states/<string:n>", strict_slashes=False)
-def html_page_display(n):
+@app.route("/cities_by_states/", strict_slashes=False)
+def html_cities_in_state():
     """
-    Display a HTML page only if n is a string
+    Display an HTML page listing all cities in a state
     """
-    states = storage.all(State).values()
-    sortd_states = sorted(states, key=lambda x: x.name)
-    return render_template('8-cities_by_states.html', n=n, states=sortd_states)
+    city = storage.all(State).values()
+    sorted_cities = sorted(city, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_cities)
 
 
 @app.teardown_appcontext
