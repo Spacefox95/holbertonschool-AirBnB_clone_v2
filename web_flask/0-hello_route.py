719b500cf887da39a7d2619751e0eaf1ea7bf3f6
Spacefox95 <nathan.raynal@gmail.com>
2024-04-11 09:17:56 +0200

    ok

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,12 +9,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# Specify the route
+""" Specify the route"""
+
+
 @app.route("/", strict_slashes=False)
 def hello():
+    """ Display Hello HBNB """
     return "Hello HBNB"
 
 
-# Specify the port
+""" Specify the port """
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
